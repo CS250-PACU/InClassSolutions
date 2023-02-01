cda06e69fac5947ceb9f5b4500b56f5cda528caf
Chris Lane <lanec@pacificu.edu>
2023-02-01 07:41:08 -0800

    Added function headers

--- RectanglesFuntional/main.cpp
+++ RectanglesFuntional/main.cpp
@@ -1,3 +1,12 @@
+//***************************************************************************
+// File name:		 main.cpp
+// Author:			 CS Faculty
+// Date:				 2/1/2023
+// Class:				 CS250
+// Assignment:	 In-class examples
+// Purpose:			 Practice with arrays of structs and functions
+//***************************************************************************
+
 #include <iostream>
 #include <fstream>
 
@@ -7,12 +16,22 @@ struct Rectangle {
 	double length, width;
 };
 
-int readRectangles(Rectangle[], const int);
-void displayRectangles(const Rectangle[], const int);
-double getArea(const Rectangle);
-Rectangle getLargest(const Rectangle[], const int);
+int readRectangles (Rectangle[], const int);
+void displayRectangles (const Rectangle[], const int);
+double getArea (const Rectangle);
+Rectangle getLargest (const Rectangle[], const int);
 
-int main() {
+//*********************************************************************
+// Function:		main
+//
+// Description: Practice with functions and arrays of structs
+// 
+// Parameters:	None
+//
+// Returned:		EXIT_SUCCESS
+//*********************************************************************
+
+int main () {
 	const int MAX = 50;
 
 	Rectangle sRectangles[MAX];
@@ -22,24 +41,35 @@ int main() {
 	count = readRectangles(sRectangles, MAX);
 
 	cout << "Here are the rectangles from the file: \n";
-	displayRectangles(sRectangles, count);
+	displayRectangles (sRectangles, count);
 
-	sLargest = getLargest(sRectangles, count);
+	sLargest = getLargest (sRectangles, count);
 	cout << "\nLargest is:\n" << "Length: " << sLargest.length
-		<< ", Width: " << sLargest.width;
+		   << ", Width: " << sLargest.width;
 
 	cout << "\n\nprogram ended successfully" << endl << endl;
 	return EXIT_SUCCESS;
 }
 
-int readRectangles(Rectangle sRectangles[], const int MAX) {
+//*********************************************************************
+// Function:		readRectangles
+//
+// Description: Reads Rectangle structs from file into array
+// 
+// Parameters:	sRectangles - Array of rectangle structs
+//							MAX - Maximum number of objects in array
+//
+// Returned:		Number of structs read into the array
+//*********************************************************************
+
+int readRectangles (Rectangle sRectangles[], const int MAX) {
 	ifstream myFile;
 	int count = 0;
-
-	myFile.open("rectangles.txt");
-	if (myFile.fail()) {
+	 
+	myFile.open ("rectangles.txt");
+	if (myFile.fail ()) {
 		cout << "Could not open file" << endl;
-		exit(EXIT_FAILURE);
+		exit (EXIT_FAILURE);
 	}
 
 	while (myFile >> sRectangles[count].length && count < MAX) {
@@ -47,30 +77,62 @@ int readRectangles(Rectangle sRectangles[], const int MAX) {
 		count++;
 	}
 
-	myFile.close();
+	myFile.close ();
 
 	return count;
 }
 
-void displayRectangles(const Rectangle sRectangles[], const int size) {
+//*********************************************************************
+// Function:		displayRectangles
+//
+// Description: Sum the numbers from 0 to 10
+// 
+// Parameters:	sRectangles - Array of rectangle structs
+//							size - number of rectangle structs in array
+//
+// Returned:		EXIT_SUCCESS
+//*********************************************************************
+
+void displayRectangles (const Rectangle sRectangles[], const int size){
 	for (int i = 0; i < size; i++) {
 		cout << "Length: " << sRectangles[i].length << ", Width: "
-			<< sRectangles[i].width << endl;
+			   << sRectangles[i].width << endl;
 	}
 }
 
-double getArea(const Rectangle sRectangle) {
+//*********************************************************************
+// Function:		getArea
+//
+// Description: Computes area of since Rectangle struct
+// 
+// Parameters:	sRecangle - Rectangle struct
+//
+// Returned:		area of rectangle struct
+//*********************************************************************
+
+double getArea (const Rectangle sRectangle) {
 	return sRectangle.length * sRectangle.width;
 }
 
-Rectangle getLargest(const Rectangle sRectangles[], const int size) {
+//*********************************************************************
+// Function:		getLargest
+//
+// Description: Determines Rectangle struct with largest area
+// 
+// Parameters:	sRectangles - Array of rectangle structs
+//							size - number of rectangle structs in array
+//
+// Returned:		Rectangle struct with the largest area
+//*********************************************************************
+
+Rectangle getLargest (const Rectangle sRectangles[], const int size) {
 	int indexLargest = 0;
-	double largestArea = getArea(sRectangles[indexLargest]);
+	double largestArea = getArea (sRectangles[indexLargest]);
 
 	for (int i = 0; i < size; i++) {
-		if (getArea(sRectangles[i]) > largestArea) {
+		if (getArea (sRectangles[i]) > largestArea) {
 			indexLargest = i;
-			largestArea = getArea(sRectangles[i]);
+			largestArea = getArea (sRectangles[i]);
 		}
 	}
 	return sRectangles[indexLargest];
